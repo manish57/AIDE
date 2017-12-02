9c4f9032dacfcecb8730d5dea0904be114bf3736
Manish Chaudhari <manish3257@gmail.com>
2017-12-02 15:34:20 -0500

    Update feedback.service.ts

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -11,7 +11,7 @@ export class FeedbackService{
 
     constructor(http: Http) {
         this.http = http;
-        this.apiKey = 'QuV-LCiIjeYAnl5TtATYL7lizSj4LplR';
+        this.apiKey = '<api-key>';
         this.feedbacksUrl = 'https://api.mlab.com/api/1/databases/kotharisdb/collections/feedbacksinfo';
     }
 
